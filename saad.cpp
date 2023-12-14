0abf1410e1b19e230d098b0e15cf89510750c564
Anas <anass3253@gmail.com>
2023-12-14 22:36:38 +0200

    after editing file

--- saad.cpp
+++ saad.cpp
@@ -4,7 +4,14 @@ using namespace std;
 void main() {
 	cout << "hello world \n";
     int x = 8;
+    int age;
+    string name;
     cout << x + 2 << endl;
 
     cout << x + 4 << endl; 
+
+    cout<<"enter age \n";
+    cin >> age;
+
+    cout << age << endl;
 }
